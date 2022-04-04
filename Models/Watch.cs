10bf756bdf4aa828bd6bb7701dd942a00cfa8270
imranmani89 <95025442+imranmani89@users.noreply.github.com>
2022-04-04 12:01:15 +0500

    Models and Solution File

---
+++ Models/Watch.cs
@@ -0,0 +1,48 @@
+﻿namespace Models
+{
+    public class Watch
+    {
+        public bool Status { get; set; }
+
+        private DateTime StartTime { get; set; }
+        private DateTime StopTime { get; set; }
+
+        public Watch()
+        {
+            Status = false; 
+        }
+
+        public bool Start()
+        {
+            if (Status)
+            {
+                Console.WriteLine("Stopwatch is already Started");
+                return false;
+            }
+
+            Status = true;
+            StartTime = DateTime.Now;
+
+            Console.WriteLine($"Stop Watch Started at {StartTime}");
+            return true;
+        }
+
+
+        public bool Stop()
+        {
+            if (!Status)
+            {
+                Console.WriteLine("Stopwatch is already Stopped");
+                return false;
+            }
+
+            Status = false;
+            StopTime = DateTime.Now;
+            Console.WriteLine($"Stop Watch Started at {StartTime}");
+            Console.WriteLine($"Stop Watch Stopped  at {StopTime}");
+            Console.WriteLine($"Total Time taken is {StopTime - StartTime}");
+
+            return true;
+        }
+    }
+}
